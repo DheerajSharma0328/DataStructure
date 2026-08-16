67fd8fc2d4952f227e3cc35812c09c9f26446885
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-08-17 01:48:32 +0530

    Second Largest and in reversed array check for odd by adding 11 in the array

--- 02. Arrays/04.reverse_array.cpp
+++ 02. Arrays/04.reverse_array.cpp
@@ -5,7 +5,7 @@ void reverse_array(int arr[] , int size);
 
 int main()
 {
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int arr[] = {1,2,3,4,5,6,7,8,9,10,11};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     reverse_array(arr , size);
---
+++ 02. Arrays/05.second_largest.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int second_largest(int arr[] , int size);
+int largest_element(int arr[] , int size);
+
+int main()
+{
+    int arr[] = {10,2,3,19,6,17,17,4,5,19};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int second_largest_element = second_largest(arr , size);
+    cout<<endl<<endl<<"Second Largest Element: "<<second_largest_element<<endl<<endl;
+    return 0;
+}
+
+int second_largest(int arr[] , int size)
+{
+    int largest = largest_element(arr , size);
+    int second_largest_element = INT_MIN;
+
+    for(int i=0; i<size; i++)
+    {
+        if(arr[i] < largest && arr[i] > second_largest_element)
+        {
+            second_largest_element = arr[i];
+        }
+    }
+
+    return second_largest_element;
+}
+
+int largest_element(int arr[] , int size)
+{
+    int largest = INT_MIN;
+    for(int i=0; i<size; i++)
+    {
+        if(arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    return largest;
+}
